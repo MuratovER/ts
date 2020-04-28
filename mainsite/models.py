2dc6936fa089cd0623d2ac99c8147fb6bf6cd756
MuratovER <eldar.muratov.3@gmail.com>
2020-04-28 04:42:20 +0300

    resolved errors

--- mainsite/models.py
+++ mainsite/models.py
@@ -28,7 +28,7 @@ class Skill(models.Model):
 #model of userSKill
 class UserSkill(models.Model): 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    skill_name = models.TextField(max_length=80)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, related_name="skill", default=0)
     level = PositiveIntegerField(blank=True, null=True)
 
 
