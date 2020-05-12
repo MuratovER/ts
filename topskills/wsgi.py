76852ae7c2b3c1b1ce804a82863a90732e6dd3a7
MuratovER <eldar.muratov.3@gmail.com>
2020-05-12 09:55:28 +0300

    solve error with wsgi

--- topskills/wsgi.py
+++ topskills/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'topskills.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainsite.settings')
 
 application = get_wsgi_application()
+
 
