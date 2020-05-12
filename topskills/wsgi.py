0f4cfb1c2c7c6a6d1abb7e1089a31ccb324909ab
MuratovER <eldar.muratov.3@gmail.com>
2020-05-12 15:46:30 +0300

    rename mainsite to topskills

--- topskills/wsgi.py
+++ topskills/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'topskills.settings')
 
 application = get_wsgi_application()
 
