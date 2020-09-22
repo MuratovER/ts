d0f7266eeaed2d5d7a1cf2b02db4c3f39d2b9345
Nodir2001 <ras.nodir@mail.ru>
2020-09-22 17:14:21 +0300

    new vision of tree4

--- mainsite/views.py
+++ mainsite/views.py
@@ -125,8 +125,14 @@ def first_chapters_self_assessment_list(request):
 def first_chapters_self_discipline(request):
     return  render(request, 'mainsite/tree/first/first_chapters_self_discipline.html',)
 
+def first_chapters_self_discipline_list(request):
+    return  render(request, 'mainsite/tree/first/first_chapters_self_discipline_list.html',)
+
 def first_chapters_aims_in_life(request):
     return  render(request, 'mainsite/tree/first/first_chapters_aims_in_life.html',)
+
+def first_chapters_aims_in_life_list(request):
+    return  render(request, 'mainsite/tree/first/first_chapters_aims_in_life_list.html',)
 
 #Second
 
