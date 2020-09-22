9c9b8897638561bf9a2604be1e241d8233cdd507
Nodir2001 <ras.nodir@mail.ru>
2020-09-22 15:58:56 +0300

    new vision of tree2

--- mainsite/views.py
+++ mainsite/views.py
@@ -116,7 +116,7 @@ def first_chapters_EditTask(request):
     return  render(request, 'mainsite/tree/first/first_chapters_EditTask.html',)
 
 def first_chapters_self_assessment_list  (request):
-    return  render(request, 'mainsite/tree/first/first_chapters_self_assessment_list  .html',)
+    return  render(request, 'mainsite/tree/first/first_chapters_self_assessment_list.html',)
 
 def first_chapters_self_assessment(request):
     return  render(request, 'mainsite/tree/first/first_chapters_self_assessment.html',)
