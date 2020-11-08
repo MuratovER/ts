b406dc693575c9276e7aa45b14b4c69b8b4864f6
Илья <dobryninis@stud.kai.ru>
2020-11-08 15:05:42 +0300

    Added first sample of API integration

--- mainsite/serializers.py
+++ mainsite/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Sphere_of_life
 
-class TodoListSerializer(serializers.ModelSerializer):
+class TodoListSerializer(serializers.ModelSerializer): # В будущем заготовка для "ежедневника"
     class Meta:
         model = Sphere_of_life
         fields = ('user', 'inside_world', 'career', 'health', 'relationships')
