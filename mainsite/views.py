6498841c35508a2a3723858ee62098a7000efb2b
Nodir2001 <rasulovnd@stud.kai.ru>
2021-02-09 16:28:16 +0300

    add about_us2

--- mainsite/views.py
+++ mainsite/views.py
@@ -44,6 +44,9 @@ def help_view(request):
 @login_required
 def skills(request):
     return render(request, 'mainsite/skills.html',)
+    
+def aboutus(request):
+    return render(request, 'mainsite/aboutus.html',)    
 
 @login_required
 def introduction_view(request):
