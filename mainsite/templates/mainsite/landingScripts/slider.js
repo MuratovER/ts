9da6ca0cf15e52a44f1043ee11a6e6de6fe50984
ayazgib <aiazgib@gmail.com>
2021-02-11 15:50:58 +0300

    Front of new landing page

---
+++ mainsite/templates/mainsite/landingScripts/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.header-slider').slick({
+    slidesToShow:1,
+    slidesToScroll:1,
+    speed: 1500,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+});
+})
+
