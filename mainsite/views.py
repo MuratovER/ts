dd35c9b0266a1fc7535ab589b91ace6c2abaf9c7
Andrey170170 <24122004@bk.ru>
2021-02-18 22:58:25 +0300

    Redid_the_lesson_disyine_6.2

--- mainsite/views.py
+++ mainsite/views.py
@@ -210,7 +210,7 @@ def first_chapters_aims_in_life(request):
     return  render(request, 'mainsite/tree/first/first_chapters_aims_in_life.html',)
 
 def first_chapters_self_aims_in_life_list(request):
-    return  render(request, 'mainsite/tree/first/first_chapters_self_aims_in_life_list.html',)
+    return  render(request, 'mainsite/tree/first/first_chapters_self_aim_in_life_list.html',)
 
 #Second
 #подключение второй главы
