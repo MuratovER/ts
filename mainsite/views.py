9da5ce5dc989c869b67789d8c64db3b7adb57f49
Max <maxim.bigin@yandex.ru>
2021-03-26 23:52:12 +0300

    added forms saves articles

--- mainsite/views.py
+++ mainsite/views.py
@@ -334,7 +334,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -345,6 +344,11 @@ def post_new(request):
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'mainsite/post_list.html', {'posts': posts})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'mainsite/post_draft_list.html', {'posts': posts})
 
 @login_required
 def post_detail(request, pk):
@@ -359,7 +363,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -371,6 +374,7 @@ def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
     return redirect('post_detail', pk=pk)
+
 
 @login_required
 def post_remove(request, pk):
@@ -403,11 +407,6 @@ def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
-
-@login_required
-def post_draft_list(request):
-    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'mainsite/post_draft_list.html', {'posts': posts})
 
 @login_required
 def add_like(request, pk):
